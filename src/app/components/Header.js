51970269dc5d8fabeca2c3711241c0a02ba5e1ce
Justin Lee <jlee201@lion.lmu.edu>
2017-10-08 18:20:39 -0700

    added more elements to html file

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,21 +5,13 @@ import styles from './HeaderStyle.css';
 
 export class Header extends React.Component {
   render(){
-    return(
-        <nav className="navbar navbar-inverse">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="#">OneUp</a>
-            </div>
-            <ul className="nav navbar-nav">
-              <li><a href="#">Home</a></li>
-              <li className="active"><a href="#">About</a></li>
-              <li><a href="#">Sign in / Create Account</a></li>
-            </ul>
-          </div>
-        </nav> 
-
-    );
+      return(
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Sign In / Create Account</a></li>
+        </ul>
+      );
   }
 }
 
