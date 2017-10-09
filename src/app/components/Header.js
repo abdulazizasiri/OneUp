d09ed2bb1cf99fcfcc8f879f1c13f362bec702d7
Khiem <klu@lion.lmu.edu>
2017-10-08 20:21:46 -0700

    small edits

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 import styles from './HeaderStyle.css';
-//import css from './HeaderStyle.css';
+import css from './HeaderStyle.css';
 
 export class Header extends React.Component {
  render(){
      return(
-       <ul className="nav navbar-nav navbar-right">
-         <li><a href="#">Home</a></li>
-         <li><a href="#">About</a></li>
-         <li><a href="#">Sign In / Create Account</a></li>
-       </ul>
-     );
+          <ul className="nav navbar-nav navbar-right">
+             <li className="active"><a href="#">Home</a></li>
+             <li><a href="#">About</a></li>
+             <li><a href="#">Sign In / Create Account</a></li>
+          </ul> 
+       
+    );
  }
 }
 
