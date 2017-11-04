46896782b5df9fd66d85cc39dd7f24052e4ee1b6
Khiem <klu@lion.lmu.edu>
2017-11-03 18:58:19 -0700

    Fixed grid formatting for user profile

--- src/app/components/Profile.js
+++ src/app/components/Profile.js
@@ -14,70 +14,110 @@ const Profile = (props) => {
 
   <div className="profile container-fluid">
     <div className="row">
-       <div className="col-xs-12 col-sm-11 col-sm-offset-1 col-md-11 col-md-offset-1">
-          <div className="profileImage">
-            <img className="img-circle" src=
-"https://www.dragonsearch.com/wp-content/themes/dragonsearch/library/images/placeholders/thumbnail_placeholder.png"/>
-          </div>
-          <div className="profileInfo" style={{marginTop: "50px", marginLeft: "150px"}}>
-             <div className="row">
-                <div className="col-xs-10">
-                   <font className="name" color="white" size="5">Justin Lee</font>
+
+       <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background:'grey', marginTop:'50px'}}>
+          <div className="row">
+            <div className="col-xs-12 col-md-2" style={{height:'170px', background:'pink'}}>
+              <div className="profileImage">
+                <img className="img-circle" src="https://www.dragonsearch.com/wp-content/themes/dragonsearch/library/images/placeholders/thumbnail_placeholder.png"/>
+              </div>
+            </div>
+
+            <div className="col-xs-12 col-md-10" style={{background:'green', marginTop:'50px'}}>
+
+              <div className="row">
+                <div className="col-xs-6 col-md-6" style={{background:'brown'}}>
+                   <div className="row">
+                      <div className="col-xs-8 col-md-8 text-center" style={{background:"yellow"}}>
+                         <font className="name" color="white" size="5">Justin Lee</font>
+                      </div>
+                   </div>
+                   <div className="row">
+                      <div className="col-xs-8 col-md-8 text-center" style={{background:"orange"}}>
+                         <font className="username" color="white" size="3">jlee201</font>
+                      </div>
+                   </div>
+                   <div className="row">
+                      <div className="col-xs-8 col-md-8 text-center" style={{background:'purple'}}>
+                        <div className="settings">
+                           <button type="button" className="settingsButton">Settings</button>
+                        </div>
+                      </div>
+                   </div>
                 </div>
-                <div className="col-xs-10">
-                   <font className="username" color="white" size="3">jlee201</font>
-                </div>
-                <div className="col-xs-10">
-                  <div className="settings">
-                     <button type="button" className="settingsButton">Settings</button>
+
+                <div className="col-xs-6 col-md-6" style={{background:'blue'}}>
+                  <div className="row">
+                    <div className="col-xs-6 col-md-6 col-md-offset-4 text-center" style={{background:'orange'}}>
+                      <div style={{marginRight: "20px"}} className="uploadClips">
+                         <button onClick={handleClick} className="uploadClipsButton"><span className="glyphicon glyphicon-plus-sign"
+                             style={{color: "red"}}></span></button><br/>
+                         <font color="white" size="3" className="uploadClipsLabel">Upload Clip</font>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="col-xs-3 col-xs-offset-8">
-                    <div style={{marginRight: "20px"}} className="uploadClips">
-                       <button onClick={handleClick} className="uploadClipsButton"><span className="glyphicon glyphicon-plus-sign"
-                           style={{color: "red"}}></span></button><br/>
-                       <font color="white" size="3" className="uploadClipsLabel">Upload Clip</font>
-                    </div>
-                </div>
-             </div>
+               </div>
+            </div>
+
+
           </div>
        </div>
     </div>
+
+    <div className="row">
+       <div className="col-xs-12 col-md-10 col-md-offset-1" style={{height: '20px',background:'black'}}>
+       </div>
+    </div>
+
+
     <div className="row" id="bio">
-       <div className="col-xs-12 col-sm-11 col-sm-offset-1 col-md-11 col-md-offset-1">
+       <div className="col-xs-12 col-md-10 col-md-offset-1" style={{height: '40px',background:'red'}}>
           <font color="white" size="4">BIO</font>
        </div>
     </div>
+
+
     <div className="row" id="socialMedia">
-       <div className="col-xs-12 col-sm-11 col-sm-offset-1 col-md-11 col-md-offset-1"
-          style={{marginTop: "25px", marginBottom: "25px"}}>
+       <div className="col-xs-12 col-md-10 col-md-offset-1"
+          style={{height: '40px', background:'yellow',marginTop: "25px", marginBottom: "25px"}}>
        <font color="white" size="4">FOLLOW ME</font>
         </div>
     </div>
+
+
     <div className="lineBorder"></div>
     <div className="row" id="uploadsSection">
-       <div className="col-xs-12 col-sm-11 col-sm-offset-1 col-md-11 col-md-offset-1"
-          style={{marginTop: "25px"}}>
+       <div className="col-xs-12 col-md-10 col-md-offset-1"
+          style={{background:'pink',marginTop: "25px"}}>
           <font color="white" size="4" className="uploads">UPLOADS</font>
           <font style={{marginLeft: "15px"}} size="2" className="uploadsCount">UPVOTE COUNT: 55</font>
        </div>
     </div>
+
+
     <div className="row" id="videoClipsSection">
-       <div className="col-xs-12 col-sm-11 col-sm-offset-1 col-md-11 col-md-offset-1" style={{marginTop: "25px"}}>
+       <div className="col-xs-12 col-md-10 col-md-offset-1" style={{ height: '50px', background:'purple'}}>
           <p>Video Clips Here</p>
        </div>
     </div>
+
+
     <div className="lineBorder" style={{marginTop: "25px"}}></div>
     <div className="row">
-       <div className="col-xs-12 col-sm-11 col-sm-offset-1 col-md-11 col-md-offset-1" style={{marginTop: "25px"}}>
+       <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background:'orange', marginTop: "25px"}}>
           <font color="white" size="4" className="upvotedVideos">UPVOTED VIDEOS</font>
        </div>
     </div>
+
+
     <div className="row">
-       <div className="col-xs-12 col-sm-11 col-sm-offset-1 col-md-11 col-md-offset-1" style={{marginTop: "25px"}}>
+       <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background:'green', height: '50px'}}>
           <p>Upvoted Clips Here</p>
        </div>
     </div>
+
+
   </div>
 );
 };
