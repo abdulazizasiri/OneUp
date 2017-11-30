cd4fe47a6f1885a79d37a91ec3f2983bfc742f77
jlee201 <jlee201@lion.lmu.edu>
2017-11-29 21:20:38 -0800

    few syntax changes

--- src/app/components/AllTime.js
+++ src/app/components/AllTime.js
@@ -1,74 +1,55 @@
+/*
 import React from 'react';
 import styles from '../CSS/HomeStyle.css';
 import css from '../CSS/HomeStyle.css';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; */
+
+var React = require('react');
+var Link = require('react-router').Link;
 
 const AllTime = (props) => {
 
-  const Home = () => {
-    props.history.push('/');
-  };
+    return (
 
-  const AllTime = () => {
-    props.history.push('/alltime');
-  };
+        <div className="home container-fluid">
+            <div className="row">
+                <div className="col-xs-12 col-md-3 col-md-offset-1 text-center"
+                    style={{background: 'black', height: '60px', top: '90px'}}>
+                    <ul style={{marginTop: "15px"}} id="week_or_alltime">
+                        <li><Link to="/"><span>WEEK</span></Link></li>
+                        <b>|</b>
+                        <li><Link to="/alltime"><span>ALL-TIME</span></Link></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-xs-12 col-md-4 col-md-offset-4 text-center"
+                    style={{background: 'black', height: '40px', top: '90px'}}>
+                    <p className="weekName">ALL-TIME</p>
+                </div>
+            </div>
 
-  return(
+            <div className="row">
+                <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background: 'black', height: '90px', top: '90px'}}>
+                    <div className="divider"><div className="inner"></div></div>
+                    <div className="text-left">1</div>
+                    <div className="text-right"><a href="#" className="glyphicon glyphicon-chevron-up"></a></div>
+                    <div className="text-right"><a href="#" className="glyphicon glyphicon-chevron-down"></a></div>
+                </div>
 
-  <div className="home container-fluid">
-    <div className="row">
-      <div className="col-xs-12 col-md-3 col-md-offset-1 text-center" style={{background:'black', height:'60px', top:'90px'}}>
-        <button onClick={Home} className="weekbtn btn-link">WEEK</button>
-        <b>|</b>
-        <button onClick={AllTime} className="all btn-link">ALL-TIME</button>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-xs-12 col-md-4 col-md-offset-4 text-center"style={{background:'black', height:'40px', top:'90px'}}>
-        <p className="weekName">ALL-TIME</p>
-      </div>
-    </div>
+                <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background: 'black', height: '90px', top: '90px'}}>
+                    <div className="divider"><div className="inner"></div></div>
+                    <div className="text-left">2</div>
+                    <div className="text-right"><a href="#" className="glyphicon glyphicon-chevron-up"></a></div>
+                    <div className="text-right"><a href="#" className="glyphicon glyphicon-chevron-down"></a></div>
+                </div>
+                <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background: 'black', height: '90px', top: '90px'}}>
+                    <div className="divider"><div className="inner"></div></div>
+                </div>
 
-    <div className="row">
-      <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background:'grey', height:'90px', top:'90px'}}>
-          <div className="divider"><div className="inner"></div></div>
-          <div className="row">
-            <div className="col-xs-2 col-md-2" style={{background:'orange',height:'90px'}}>
-              <div className="text-left">1</div>
             </div>
-            <div className="col-xs-2 col-md-2" style={{background:'pink', height:'90px'}}>
-            </div>
-            <div className="col-xs-6 col-md-6" style={{background:'red', height:'90px'}}>
-            </div>
-            <div className="col-xs-2 col-md-2" style={{background:'purple', height:'90px'}}>
-              <div className="text-right"><a href="#" className="glyphicon glyphicon-chevron-up"></a></div>
-              <div className="text-right"><a href="#" className="glyphicon glyphicon-chevron-down"></a></div>
-            </div>
-          </div>
-      </div>
-
-      <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background:'grey', height:'90px', top:'90px'}}>
-          <div className="divider"><div className="inner"></div></div>
-          <div className="row">
-            <div className="col-xs-2 col-md-2" style={{background:'orange',height:'90px'}}>
-              <div className="text-left">2</div>
-            </div>
-            <div className="col-xs-2 col-md-2" style={{background:'pink', height:'90px'}}>
-            </div>
-            <div className="col-xs-6 col-md-6" style={{background:'red', height:'90px'}}>
-            </div>
-            <div className="col-xs-2 col-md-2" style={{background:'purple', height:'90px'}}>
-              <div className="text-right"><a href="#" className="glyphicon glyphicon-chevron-up"></a></div>
-              <div className="text-right"><a href="#" className="glyphicon glyphicon-chevron-down"></a></div>
-            </div>
-          </div>
-      </div>
-      <div className="col-xs-12 col-md-10 col-md-offset-1" style={{background:'black', height:'90px', top:'90px'}}>
-          <div className="divider"><div className="inner"></div></div>
-      </div>
-
-    </div>
-  </div>
-);
+        </div>
+    );
 };
-export default AllTime
+
+module.exports = AllTime;
