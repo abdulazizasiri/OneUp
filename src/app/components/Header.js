7bc0800e254b68f5980036c50a0d9e8d434b2c44
jlee201 <jlee201@lion.lmu.edu>
2017-11-29 21:21:36 -0800

    few syntax changes and added html 'logic'

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,36 +1,89 @@
-import React from 'react';
+var React = require('react');
+var Link = require('react-router').Link;
+
+/*
 import styles from '../CSS/HeaderStyle.css';
 import css from '../CSS/HeaderStyle.css';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <nav className="navbar navbar-inverse navbar-fixed-top app-navbar" role="navigation">
-        <div className="container">
+<li><Link to='/'>Home</Link></li>
+<li><Link to='/about'>About</Link></li>
+<li><Link to='/profile'>Profile</Link></li>
+<li><Link to='/signin'>Sign In / Create Account</Link></li>
+<li><Link to='/signin'>Sign Out</Link></li>
 
-            <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="/">OneUp</a>
-            </div>
+{
+    this.props.tabs.map(function(tab) {
+        var route;
+        if (tab === 'Home') {
+            route = '/';
+        } else if (tab === 'About') {
+            route = '/about';
+        } else if (tab === 'Sign In/Create Account') {
+            route = '/signin';
+        } else if (tab === 'Profile') {
+            route = '/profile';
+        } else if (tab === 'Sign Out') {
+            route = '/signin';
+        }
+        return <li key={tab}><Link to={route}>{tab}</Link></li>;
+    })
+}
+
+<li><Link to='/profile'>Profile</Link></li>
 
 
-            <div className="collapse navbar-collapse pull-right" id="navbar" >
-                <ul className="nav navbar-nav navbar-right">
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/profile'>Profile</Link></li>
-                    <li><Link to='/signin'>Sign In / Create Account</Link></li>
-                </ul>
-            </div>
 
-        </div>
-    </nav>
-  </header>
-)
+<li><Link to='/'>Home</Link></li>
+<li><Link to='/about'>About</Link></li>
+<li><Link to='/signin'>Sign In / Create Account</Link></li>
 
-export default Header
+*/
+
+const Header = (props) => {
+
+    return (
+        <header>
+            <nav className="navbar navbar-inverse navbar-fixed-top app-navbar" role="navigation">
+                <div className="container">
+
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="/">OneUp</a>
+                    </div>
+
+                    <div className="collapse navbar-collapse pull-right" id="navbar" >
+                        <ul className="nav navbar-nav navbar-right">
+                            {
+                                props.tabs.map(function(tab) {
+                                    var route;
+                                    if (tab === 'Home') {
+                                        route = '/';
+                                    } else if (tab === 'About') {
+                                        route = '/about';
+                                    } else if (tab === 'Sign In/Create Account') {
+                                        route = '/signin';
+                                    } else if (tab === 'Profile') {
+                                        route = '/profile';
+                                    }
+
+                                    return <li key={tab}><Link to={route}>{tab}</Link></li>;
+                                })
+                            }
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+
+module.exports = Header;
+
