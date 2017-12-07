7820d81d2e8d3bb5a16cc0525dc9eaece94cee19
jlee201 <jlee201@lion.lmu.edu>
2017-12-06 18:50:39 -0800

    restructured the video component

--- src/app/components/Video.js
+++ src/app/components/Video.js
@@ -1,29 +1,103 @@
-import React from 'react';
-import styles from '../CSS/VideoStyle.css';
-import css from '../CSS/VideoStyle.css';
-import { Link } from 'react-router-dom';
+var React = require('react');
+var Modal = require('./Modal.js');
 
-const Video = () => (
-  <div className="modal fade">
-  <div className="modal-dialog" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title">Modal title</h5>
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div className="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-primary">Save changes</button>
-        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+class Video extends React.Component {
 
-)
+    constructor(props) {
+        super(props);
+        this.state = { isModalOpen: false };
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
 
-export default Video
+    openModal() {
+        this.setState({ isModalOpen: true });
+    }
+
+    closeModal() {
+        this.setState({ isModalOpen: false });
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-xs-12 col-md-10 col-md-offset-1"
+                    style={{background: '', height: '90px', top: '90px'}}>
+                    <div className="divider"><div className="inner"></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-2 col-md-1" style={{background: '', height: '90px'}}>
+                            <div className="text-left">{this.props.rank}</div>
+                        </div>
+
+                        <div className="col-xs-2 col-md-2" style={{background: '', height: '90px'}}>
+                            <button onClick={this.openModal} className="video btn-primary"
+                                style={{height: '80px', width: '120px', position: 'absolute', top: '5px'}}>Video</button>
+                            <div className="center-modalSize" style={{position: 'relative', top: '150px', left: '110px'}}>
+                                <Modal isOpen={this.state.isModalOpen} onClose={() => this.closeModal}>
+                                    <div className="col-xs-12 col-md-12" style={{background: 'pink'}}>
+                                        <h1 style={{color: 'white'}}>WEEK 1 -- RANKED #1
+                                            <button onClick={this.closeModal}
+                                                className="glyphicon glyphicon-remove"></button>
+                                        </h1>
+                                    </div>
+                                    <div>
+                                        <div className="col-xs-12 col-md-12 text-center" style={{background: ''}}>
+                                            <video width="500" height="240" src={this.props.metadata.videoPath} controls>
+                                            </video>
+                                        </div>
+                                    </div>
+                                    <div className="col-xs-12 col-md-4 text-center"
+                                        style={{background: 'red', top: '20px', height: '90px'}}>
+                                        <p> {this.props.metadata.title} <br />
+                                        by: {this.props.metadata.username} <br /> posted: date </p>
+                                    </div>
+                                    <div className="col-xs-12 col-md-8"
+                                        style={{background: '', top: '20px', height: '90px', color: 'white'}}>
+                                        <p> {this.props.metadata.description} </p>
+                                    </div>
+                                </Modal>
+                            </div>
+
+                        </div>
+
+
+                        <div className="col-xs-3 col-md-2" style={{background: 'black', height: '90px'}}>
+                            <div className = "videoInfo" style={{position: 'absolute', top: '12px'}}>
+                                <p color="white"> {this.props.metadata.title} <br />
+                                by: {this.props.metadata.username} <br /> posted: date</p>
+                            </div>
+                        </div>
+                        <div className="col-xs-3 col-md-5" style={{background: 'black', height: '90px'}}>
+                            <div className="description" style={{position: 'absolute', top: '12px'}}>
+                                <p> {this.props.metadata.description} </p>
+                            </div>
+                        </div>
+                        <div className="col-xs-2 col-md-2 text-center" style={{background: '', height: '90px'}}>
+                            <div className = "votingArrows" style={{position: 'absolute', left: '60px'}}>
+                                <div className="text-right"><a href="#"
+                                    className="glyphicon glyphicon-chevron-up"></a>
+                                </div>
+                                <div className="col xs-12 col-md-12 text-center"
+                                    style ={{background: '', position: 'relative', top: '2px', right: '31px'}}>
+                                    <span style={{color: 'white', fontSize: '15px'}}>{this.props.metadata.totalUpvotes}</span>
+                                </div>
+                                <div className="text-right"><a href="#"
+                                    className="glyphicon glyphicon-chevron-down"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-xs-12 col-md-10 col-md-offset-1"
+                    style={{background: '', height: '0px', top: '90px'}}>
+                    <div className="divider"><div className="inner"></div></div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+module.exports = Video;
