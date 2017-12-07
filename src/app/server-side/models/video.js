17875d331a88078ffc07ffa68d6c0b3e2f1d416e
jlee201 <jlee201@lion.lmu.edu>
2017-12-06 18:55:15 -0800

    save video now saves to user schema

--- src/app/server-side/models/video.js
+++ src/app/server-side/models/video.js
@@ -1,8 +1,15 @@
 var mongoose = require('mongoose');
+var User = require('../models/user.js');
 
 mongoose.connect('mongodb://test:test@ds113660.mlab.com:13660/oneup');
 
 var VideoSchema = mongoose.Schema({
+    videoPath: {
+        type: String
+    },
+    totalUpvotes: {
+        type: Number
+    },
     title: {
         type: String
     },
@@ -11,23 +18,45 @@ var VideoSchema = mongoose.Schema({
     },
     username: {
         type: String
-    },
-    userId: {
-        type: String
     }
 });
 
 var Video = module.exports = mongoose.model('Video', VideoSchema);
 
 module.exports.saveVideo = function(newVideo, user) {
+    var videofile = newVideo.videofile;
     var title = newVideo.title;
     var description = newVideo.description;
     var username = user.username;
     var userId = user.id;
-    var vid = Video({title: title, description: description, username: username, userId: userId}).save(function(err) {
+    var vid = Video(
+        {
+            videoFile: videofile,
+            totalUpvotes: 0,
+            title: title,
+            description: description,
+            username: username,
+            userId: userId
+        }).save(function(err) {
         if (err) {
             throw err;
         }
+        var updatedUploads;
+        User.getUserByUsername(username, function(err, user) {
+            if (err) {
+                throw err;
+            }
+            updatedUploads = user.uploads;
+            updatedUploads.push(videofile);
+            User.findOneAndUpdate(
+                {username: username},
+                {$set: {uploads: updatedUploads}},
+                function(err, doc) {
+                    if (err) {
+                        throw err;
+                    }
+                });
+        });
         console.log("video saved");
     });
 };
