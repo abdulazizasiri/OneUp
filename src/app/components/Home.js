a5491c576387021c14a4b7699f827b0c60a9b14e
jlee201 <jlee201@lion.lmu.edu>
2017-12-06 18:48:24 -0800

    Restructured Home.js

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,76 +1,43 @@
 var React = require('react');
 var Link = require('react-router').Link;
+var Video = require('./Video.js');
 
-const Home = () => {
+class Home extends React.Component {
 
-    return (
-        <div className="home container-fluid">
-            <div className="row">
-                <div className="col-xs-12 col-md-3 col-md-offset-1 text-center"
-                    style={{background: 'black', height: '60px', top: '90px'}}>
-                    <ul style={{marginTop: "15px"}} id="week_or_alltime">
-                        <li><Link to="/"><span>WEEK</span></Link></li>
-                        <b>|</b>
-                        <li><Link to="/alltime"><span>ALL-TIME</span></Link></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-xs-12 col-md-4 col-md-offset-4 text-center"
-                    style={{background: 'black', height: '40px', top: '90px'}}>
-                    <p className="weekName">WEEK 28</p>
-                </div>
-            </div>
+    constructor(props) {
+        super(props);
+    }
 
-            <div className="row">
-                <div className="col-xs-12 col-md-10 col-md-offset-1"
-                    style={{background: '', height: '90px', top: '90px'}}>
-                    <div className="divider"><div className="inner"></div></div>
-                    <div className="row">
-                        <div className="col-xs-2 col-md-2" style={{background: '', height: '90px'}}>
-                            <div className="text-left">1</div>
-                        </div>
-                        <div className="col-xs-2 col-md-2" style={{background: '', height: '90px'}}>
-                        </div>
-                        <div className="col-xs-6 col-md-6" style={{background: '', height: '90px'}}>
-                        </div>
-                        <div className="col-xs-2 col-md-2" style={{background: '', height: '90px'}}>
-                            <div className="text-right"><a href="#"
-                                className="glyphicon glyphicon-chevron-up"></a></div>
-                            <div className="text-right"><a href="#"
-                                className="glyphicon glyphicon-chevron-down"></a></div>
-                        </div>
+    render() {
+
+        var videoComponents = this.props.videos.map(function(videodata, index) {
+            var rank = index + 1;
+            return (
+                <Video key={index} rank={rank} metadata={videodata}/>
+            );
+        });
+        return (
+            <div className="home container-fluid">
+                <div className="row">
+                    <div className="col-xs-12 col-md-3 col-md-offset-1 text-center"
+                        style={{background: '', height: '60px', top: '90px'}}>
+                        <ul style={{marginTop: "15px"}} id="week_or_alltime">
+                            <li><Link to="/"><span>WEEK</span></Link></li>
+                            <b>|</b>
+                            <li><Link to="/alltime"><span>ALL-TIME</span></Link></li>
+                        </ul>
                     </div>
                 </div>
-
-                <div className="col-xs-12 col-md-10 col-md-offset-1"
-                    style={{background: '', height: '90px', top: '90px'}}>
-                    <div className="divider"><div className="inner"></div></div>
-                    <div className="row">
-                        <div className="col-xs-2 col-md-2" style={{background: '', height: '90px'}}>
-                            <div className="text-left">2</div>
-                        </div>
-                        <div className="col-xs-2 col-md-2" style={{background: '', height: '90px'}}>
-                        </div>
-                        <div className="col-xs-6 col-md-6" style={{background: '', height: '90px'}}>
-                        </div>
-                        <div className="col-xs-2 col-md-2" style={{background: '', height: '90px'}}>
-                            <div className="text-right"><a href="#"
-                                className="glyphicon glyphicon-chevron-up"></a></div>
-                            <div className="text-right"><a href="#"
-                                className="glyphicon glyphicon-chevron-down"></a></div>
-                        </div>
+                <div className="row">
+                    <div className="col-xs-12 col-md-4 col-md-offset-4 text-center"
+                        style={{background: '', height: '40px', top: '90px'}}>
+                        <p className="weekName">WEEK 1</p>
                     </div>
                 </div>
-
-                <div className="col-xs-12 col-md-10 col-md-offset-1"
-                    style={{background: '', height: '90px', top: '90px'}}>
-                    <div className="divider"><div className="inner"></div></div>
-                </div>
+                {videoComponents}
             </div>
-        </div>
-    );
-};
-
+        );
+    }
+}
 
 module.exports = Home;
